ceaf9880d37e02d3370764f9086d742394ade64d
Shelby <119420587+Saulnaya-street@users.noreply.github.com>
2024-11-05 01:22:13 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+	// Выводим результат вызова метода Distance
+	//swimmingInfo := swimming.TrainingInfo()
+	//fmt.Printf("swimming - Дистанция: %.2f км, Средняя скорость: %.2f км/мин,Калории: %.2f ккал\n",
+	//	swimmingInfo.Distance, swimmingInfo.MeanSpeed, swimmingInfo.Calories)
+	//walkingInfo := walking.TrainingInfo()
+	//fmt.Printf("walking - Дистанция: %.2f км, Средняя скорость: %.2f км/мин,Калории: %.2f ккал\n",
+	//	walkingInfo.Distance, walkingInfo.MeanSpeed, walkingInfo.Calories)
+	//runningInfo := running.TrainingInfo()
+	//fmt.Printf("running - Дистанция: %.2f км, Средняя скорость: %.2f км/мин,Калории: %.2f ккал\n",
+	//	runningInfo.Distance, runningInfo.MeanSpeed, runningInfo.Calories)
+}
